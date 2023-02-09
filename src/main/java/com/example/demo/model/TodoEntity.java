ef80bba2453628b85e1253b157c0039995f62d0d
2hongbi <foxgirin950@gmail.com>
2023-02-09 12:57:22 +0900

    feat: TodoEntity 어노테이션 추가

--- src/main/java/com/example/demo/model/TodoEntity.java
+++ src/main/java/com/example/demo/model/TodoEntity.java
@@ -4,13 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
+@Table(name = "Todo")
 public class TodoEntity {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "uuid")
     private String id; // 오브젝트 아이디
     private String userId; // 이 오브젝트를 생성한 유저의 아이디
     private String title; // 투두(todo) 타이틀
